19bc9e7adc19da154542ced3584656bfe3d7dda1
Yinyin L <yinyinl@gmail.com>
2019-04-01 01:16:50 +0800

    capture heading code indicator

--- parse.go
+++ parse.go
@@ -1,6 +1,7 @@
 package literalcodegen
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 
@@ -21,8 +22,35 @@ func newMarkdownParseSpace() (result *markdownParseSpace) {
 	return
 }
 
+func (w *markdownParseSpace) stateHeading1(token markdown.Token) (nextCallable markdownParseCallable, err error) {
+	if textToken, ok := token.(*markdown.Inline); ok {
+		if textToken.Content == "Heading Code" {
+			w.currentNode = w.result.NewHeadingCode()
+			log.Printf("having heading code node")
+		} else {
+			w.currentNode = w.result.NewLiteralConstant()
+			log.Printf("having literal constant node")
+		}
+		return w.stateZero, nil
+	}
+	return nil, fmt.Errorf("unexpected markdown node: %T %#v", token, token)
+}
+
+func (w *markdownParseSpace) checkHeading(token *markdown.HeadingOpen) (nextCallable markdownParseCallable, err error) {
+	if token.HLevel == 1 {
+		return w.stateHeading1, nil
+	}
+	return nil, nil
+}
+
 func (w *markdownParseSpace) stateZero(token markdown.Token) (nextCallable markdownParseCallable, err error) {
 	log.Printf("> %T: %#v", token, token)
+	switch t := token.(type) {
+	case *markdown.HeadingOpen:
+		return w.checkHeading(token.(*markdown.HeadingOpen))
+	default:
+		log.Printf("skipped markdown node: %v, %v", t, token)
+	}
 	return nil, nil
 }
 
