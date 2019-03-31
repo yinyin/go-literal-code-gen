532f7fce250d8a5f3edca54419d1f400dd89bab7
Yinyin L <yinyinl@gmail.com>
2019-03-31 19:24:48 +0800

    add command interface

---
+++ cmd/go-literal-code-gen/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+
+	literalcodegen "github.com/yinyin/go-literal-code-generator"
+)
+
+func main() {
+	inputFilePath, outputFilePath, err := parseCommandParam()
+	if nil != err {
+		log.Fatalf("ERR: cannot have required parameters: %v", err)
+		return
+	}
+	log.Printf("Input: %v", inputFilePath)
+	log.Printf("Output: %v", outputFilePath)
+	err = literalcodegen.ParseMarkdown(inputFilePath)
+	if nil != err {
+		log.Fatalf("ERR: parsing Markdown input failed: %v", err)
+		return
+	}
+}
