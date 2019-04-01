48aa3f89d5f3db7a208fbe3731e9033c090b4acc
Yinyin L <yinyinl@gmail.com>
2019-04-01 18:21:56 +0800

    add parse logic

--- parse.go
+++ parse.go
@@ -13,6 +13,7 @@ type markdownParseCallable func(token markdown.Token) (markdownParseCallable, er
 type markdownParseSpace struct {
 	result      LiteralCode
 	currentNode *LiteralEntry
+	replaceRule *ReplaceRule
 }
 
 func newMarkdownParseSpace() (result *markdownParseSpace) {
@@ -31,7 +32,7 @@ func (w *markdownParseSpace) stateHeading1(token markdown.Token) (nextCallable m
 		}
 		return w.stateZero, nil
 	}
-	return nil, fmt.Errorf("unexpected markdown node: %T %#v", token, token)
+	return nil, fmt.Errorf("unexpected markdown node (L1-H): %T %#v", token, token)
 }
 
 func (w *markdownParseSpace) checkHeading(token *markdown.HeadingOpen) (nextCallable markdownParseCallable, err error) {
@@ -41,15 +42,119 @@ func (w *markdownParseSpace) checkHeading(token *markdown.HeadingOpen) (nextCall
 	return nil, nil
 }
 
+func (w *markdownParseSpace) stateReplaceRuleZero(token markdown.Token) (nextCallable markdownParseCallable, err error) {
+	node, ok := token.(*markdown.CodeInline)
+	if !ok {
+		log.Printf("- skipped: replace-rule (L2-R-0): %T, %v", token, token)
+		return
+	}
+	txt := node.Content
+	if nil == w.replaceRule.RegexTrap {
+		err = w.replaceRule.setRegexTrap(txt)
+	} else if -1 == w.replaceRule.GroupIndex {
+		err = w.replaceRule.setGroupIndex(txt)
+	} else {
+		w.replaceRule.setReplacementText(txt)
+	}
+	return
+}
+
+func (w *markdownParseSpace) stateReplaceRule(token markdown.Token) (nextCallable markdownParseCallable, err error) {
+	switch token.(type) {
+	case *markdown.Inline:
+		node := token.(*markdown.Inline)
+		w.feedTokens(w.stateReplaceRuleZero, node.Children)
+	case *markdown.BulletListClose:
+		if nil != w.replaceRule.RegexTrap {
+			w.currentNode.appendReplaceRule(w.replaceRule)
+		}
+		w.replaceRule = nil
+		return w.stateOptionItem, nil
+	default:
+		log.Printf("- skipped: replace-rule (L2-R): %T, %v", token, token)
+	}
+	return
+}
+
+func (w *markdownParseSpace) stateOptionItemConst(token markdown.Token) (nextCallable markdownParseCallable, err error) {
+	node, ok := token.(*markdown.CodeInline)
+	if !ok {
+		log.Printf("- skipped: option (L1-0-const): %T, %v", token, token)
+		return
+	}
+	w.currentNode.Name = node.Content
+	return
+}
+
+func (w *markdownParseSpace) stateOptionItemBuilder(token markdown.Token) (nextCallable markdownParseCallable, err error) {
+	node, ok := token.(*markdown.CodeInline)
+	if !ok {
+		log.Printf("- skipped: option (L1-0-builder): %T, %v", token, token)
+		return
+	}
+	txt := node.Content
+	if "" == w.currentNode.Name {
+		w.currentNode.Name = txt
+	} else {
+		w.currentNode.Parameters = append(w.currentNode.Parameters, txt)
+	}
+	return
+}
+
+func (w *markdownParseSpace) stateOptionItemZero(token markdown.Token) (nextCallable markdownParseCallable, err error) {
+	node, ok := token.(*markdown.CodeInline)
+	if !ok {
+		log.Printf("- skipped: option (L1-0): %T, %v", token, token)
+		return
+	}
+	switch node.Content {
+	case "const":
+		w.currentNode.TranslationMode = TranslateAsConst
+		nextCallable = w.stateOptionItemConst
+	case "builder":
+		w.currentNode.TranslationMode = TranslateAsBuilder
+		nextCallable = w.stateOptionItemBuilder
+	case "replace":
+		w.replaceRule = newReplaceRule()
+		// return w.stateOptionItemReplace, nil
+	case "strip-spaces":
+		w.currentNode.TrimSpace = true
+	case "preserve-new-line":
+		w.currentNode.PreserveNewLine = true
+	case "tail-new-line":
+		w.currentNode.TailNewLine = true
+	default:
+		log.Printf("** unknown option command (L1-0): %v", node.Content)
+	}
+	return
+}
+
+func (w *markdownParseSpace) stateOptionItem(token markdown.Token) (nextCallable markdownParseCallable, err error) {
+	switch token.(type) {
+	case *markdown.Inline:
+		node := token.(*markdown.Inline)
+		w.feedTokens(w.stateOptionItemZero, node.Children)
+	case *markdown.ListItemClose:
+		return w.stateZero, nil
+	case *markdown.BulletListOpen:
+		return w.stateReplaceRule, nil
+	default:
+		log.Printf("- skipped option (L1): %T, %#v", token, token)
+	}
+	return
+}
+
 func (w *markdownParseSpace) stateZero(token markdown.Token) (nextCallable markdownParseCallable, err error) {
-	log.Printf("> %T: %#v", token, token)
-	switch t := token.(type) {
+	switch token.(type) {
 	case *markdown.HeadingOpen:
 		return w.checkHeading(token.(*markdown.HeadingOpen))
-	case *markdown.BulletListOpen:
-
+	case *markdown.ListItemOpen:
+		return w.stateOptionItem, nil
+	case *markdown.Fence:
+		fenceToken := token.(*markdown.Fence)
+		w.currentNode.AppendContent(fenceToken.Content)
 	default:
-		log.Printf("skipped markdown node: %v, %v", t, token)
+		log.Printf("- skipped: markdown (L0): %T, %#v", token, token)
 	}
 	return nil, nil
 }
@@ -67,7 +172,7 @@ func (w *markdownParseSpace) feedTokens(startCallable markdownParseCallable, tok
 }
 
 // ParseMarkdown parse input file as literal definition in markdown form.
-func ParseMarkdown(filePath string) (err error) {
+func ParseMarkdown(filePath string) (code *LiteralCode, err error) {
 	buf, err := ioutil.ReadFile(filePath)
 	if nil != err {
 		return
@@ -78,5 +183,5 @@ func ParseMarkdown(filePath string) (err error) {
 	if err = work.feedTokens(work.stateZero, tokens); nil != err {
 		return
 	}
-	return nil
+	return &work.result, nil
 }
