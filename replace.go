2fa41e986516f23bdc4beae866695b9f5170195f
Yinyin L <yinyinl@gmail.com>
2019-04-06 19:02:36 +0800

    implements text replace

--- replace.go
+++ replace.go
@@ -1,6 +1,7 @@
 package literalcodegen
 
 import (
+	"fmt"
 	"regexp"
 	"strconv"
 	"strings"
@@ -40,10 +41,112 @@ func (rule *ReplaceRule) setGroupIndex(v string) (err error) {
 		return
 	}
 	rule.GroupIndex = int(idx)
+	if rule.GroupIndex < 0 {
+		rule.GroupIndex = 0
+	}
 	return nil
 }
 
 func (rule *ReplaceRule) setReplacementCode(v string) (err error) {
 	rule.ReplacementCode = v
 	return nil
+}
+
+func (rule *ReplaceRule) doReplace(textLine string) (result *ReplaceResult, err error) {
+	aux := rule.RegexTrap.FindStringSubmatchIndex(textLine)
+	if nil == aux {
+		return
+	}
+	indexIdx := rule.GroupIndex * 2
+	if (indexIdx + 1) >= len(aux) {
+		err = fmt.Errorf("given match group index (%d) out of range (%d/2): rule=%#v, %v", rule.GroupIndex, len(aux), rule.RegexTrap, textLine)
+		return
+	}
+	replaceStart := aux[indexIdx]
+	suffixStart := aux[indexIdx]
+	result = &ReplaceResult{
+		PrefixLiteral: textLine[0:replaceStart],
+		ReplacedCode:  rule.ReplacementCode,
+		SuffixLiteral: textLine[suffixStart:],
+	}
+	return result, nil
+}
+
+// ReplaceResult is thre result of one replace operation.
+type ReplaceResult struct {
+	PrefixLiteral string
+	ReplacedCode  string
+	SuffixLiteral string
+}
+
+func (r *ReplaceResult) isEmpty() bool {
+	if ("" == r.PrefixLiteral) && ("" == r.ReplacedCode) && ("" == r.SuffixLiteral) {
+		return true
+	}
+	return false
+}
+
+func (r *ReplaceResult) isSimpleLiteral() bool {
+	if ("" != r.PrefixLiteral) && ("" == r.ReplacedCode) && ("" == r.SuffixLiteral) {
+		return true
+	}
+	return false
+}
+
+func (r *ReplaceResult) runReplaceWith(rule *ReplaceRule) (prefixResult, replacedResult, suffixResult *ReplaceResult, err error) {
+	replacedResult = &ReplaceResult{
+		PrefixLiteral: r.PrefixLiteral,
+		ReplacedCode:  r.ReplacedCode,
+		SuffixLiteral: r.SuffixLiteral,
+	}
+	if "" != r.PrefixLiteral {
+		if prefixResult, err = rule.doReplace(r.PrefixLiteral); nil != err {
+			return
+		} else if nil != prefixResult {
+			replacedResult.PrefixLiteral = ""
+		}
+	}
+	if "" != r.SuffixLiteral {
+		if suffixResult, err = rule.doReplace(r.SuffixLiteral); nil != err {
+			return
+		} else if nil != suffixResult {
+			replacedResult.SuffixLiteral = ""
+		}
+	}
+	if replacedResult.isEmpty() {
+		replacedResult = nil
+	}
+	return
+}
+
+func doReplace(rules []*ReplaceRule, text string) (result []*ReplaceResult, err error) {
+	result = []*ReplaceResult{
+		{
+			PrefixLiteral: text,
+			ReplacedCode:  "",
+			SuffixLiteral: "",
+		}}
+	for _, rule := range rules {
+		buffer := result
+		result = nil
+		for _, aux := range buffer {
+			prefixResult, replacedResult, suffixResult, err := aux.runReplaceWith(rule)
+			if nil != err {
+				return nil, err
+			}
+			if nil != prefixResult {
+				result = append(result, prefixResult)
+			}
+			if nil != replacedResult {
+				result = append(result, replacedResult)
+			}
+			if nil != suffixResult {
+				result = append(result, suffixResult)
+			}
+		}
+	}
+	if (1 == len(result)) && result[0].isSimpleLiteral() {
+		return nil, nil
+	}
+	return result, nil
 }
