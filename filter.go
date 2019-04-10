161561e98e08b1e3567f4e2bb6d4da374f75b07a
Yinyin L <yinyinl@gmail.com>
2019-04-10 16:57:23 +0800

    add filter callable for SQL

---
+++ filter.go
@@ -0,0 +1,53 @@
+package literalcodegen
+
+import (
+	"strings"
+	"unicode"
+)
+
+type languageContentFilter func(codeContent, filterArgs []string) (result []string, err error)
+
+func parseSQLFilterArgs(filterArgs []string) (removeComments bool) {
+	removeComments = true
+	for _, arg := range filterArgs {
+		if arg == "keep-comment" {
+			removeComments = false
+		}
+	}
+	return
+}
+
+func sqlContentFilter(codeContent, filterArgs []string) (result []string, err error) {
+	removeComments := parseSQLFilterArgs(filterArgs)
+	lastLineIndex := len(codeContent) - 1
+	for idx, line := range codeContent {
+		if removeComments {
+			stripped := strings.TrimLeftFunc(line, unicode.IsSpace)
+			if strings.HasPrefix(stripped, "--") || strings.HasPrefix(stripped, "/*") {
+				continue
+			}
+		}
+		if idx == lastLineIndex {
+			line = strings.TrimRightFunc(line, func(r rune) bool {
+				if (r == ';') || unicode.IsSpace(r) {
+					return true
+				}
+				return false
+			})
+		}
+		result = append(result, line)
+	}
+	return
+}
+
+func runLanaguageFilter(langType string, codeContent, filterArgs []string) (result []string, err error) {
+	var filterCallable languageContentFilter
+	switch langType {
+	case "sql":
+		filterCallable = sqlContentFilter
+	}
+	if nil == filterCallable {
+		return codeContent, nil
+	}
+	return filterCallable(codeContent, filterArgs)
+}
